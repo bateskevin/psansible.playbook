0aadad4088b7aad221075e75206c73e8bcc756e1
Kevin Bates <31886373+bateskevin@users.noreply.github.com>
2022-03-28 15:24:55 +0200

    Update Invoke-PSAnsiblePlaybook.tests.ps1

--- Tests/Invoke-PSAnsiblePlaybook.tests.ps1
+++ Tests/Invoke-PSAnsiblePlaybook.tests.ps1
@@ -1,9 +1,29 @@
 # Importing Module
 Import-Module $PSScriptRoot\..\psansible.playbook\psansible.playbook.psd1 -Force
 
-Describe "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Basefunctionality]" {
-    It "should be true" {
+Describe "Testing Parameter" {
+    BeforeAll {
+        # Run Ansible Playbook
         $res = invoke-PSAnsiblePlaybook -Playbook /etc/ansible/ps.yml -i /etc/ansible/hosts
+    }
+    
+    It "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Parameter] Playbook" {
         $res.playbook | should -be "/etc/ansible/ps.yml"
+    }
+    
+    It "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Parameter] ParamName" {
+        $res.param.ParamName | should -be "-i"
+    }
+    
+    It "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Parameter] ParamValue" {
+        $res.param.ParamName | should -be "/etc/ansible/hosts"
+    }
+    
+    It "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Parameter] IsBool" {
+        $res.param.ParamName | should -be $false
+    }
+    
+    It "[PSAnsiblePlaybook][Function][Invoke-PSAnsiblePlaybook][Parameter] ExcecutionString" {
+        $res.param.ExcecutionString | should -be "ansible-playbook /etc/ansible/ps.yml -i /etc/ansible/hosts"
     }
 }
